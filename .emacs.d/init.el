42cea7b10533d936ef7b9a78e2f6ff15164935ed
Peter Hebden <peterhebden6@gmail.com>
2022-02-08 06:16:19 +0000

    Configure for React

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,7 +43,7 @@
    '("3d2e532b010eeb2f5e09c79f0b3a277bfc268ca91a59cdda7ffd056b868a03bc" "c8b83e7692e77f3e2e46c08177b673da6e41b307805cd1982da9e2ea2e90e6d7" default))
  '(frame-brackground-mode 'dark)
  '(package-selected-packages
-   '(flycheck multiple-cursors mozc scss-mode magit gruber-darker-theme typescript-mode vue-mode company-lsp lsp-mode haskell-mode monokai-pro-theme smex)))
+   '(prettier company rjsx-mode tide web-mode yasnippet flycheck multiple-cursors mozc scss-mode magit gruber-darker-theme typescript-mode vue-mode company-lsp lsp-mode haskell-mode monokai-pro-theme smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -93,3 +93,73 @@
 (global-display-line-numbers-mode)
 
 (global-flycheck-mode)
+
+;; auto revert files on change
+(global-auto-revert-mode t)
+
+;; non-verbose dired auto revert
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
+(require 'rjsx-mode)
+(require 'typescript-mode)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(require 'tide)
+(require 'company)
+(require 'yasnippet)
+(require 'prettier)
+
+ ;; tide def func: (https://dev.to/viglioni/how-i-set-up-my-emacs-for-typescript-3eeh)
+ (defun tide-setup-hook ()
+    (tide-setup)
+    (eldoc-mode)
+    (tide-hl-identifier-mode +1)
+    (setq web-mode-enable-auto-quoting nil)
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-attr-indent-offset 2)
+    (setq web-mode-attr-value-indent-offset 2)
+    (setq lsp-eslint-server-command '("node" (concat (getenv "HOME") "/var/src/vscode-eslint/server/out/eslintServer.js") "--stdio"))
+    (set (make-local-variable 'company-backends)
+         '((company-tide company-files :with company-yasnippet)
+           (company-dabbrev-code company-dabbrev))))
+
+;; hooks
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+
+;; use rjsx-mode for .js* files except json and use tide with rjsx
+(add-to-list 'auto-mode-alist '("\\.js.*$" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(add-hook 'rjsx-mode-hook 'tide-setup-hook)
+
+
+;; web-mode extra config
+(add-hook 'web-mode-hook 'tide-setup-hook
+          (lambda () (pcase (file-name-extension buffer-file-name)
+                  ("tsx" ('tide-setup-hook))
+                  (_ (my-web-mode-hook)))))
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+(add-hook 'web-mode-hook 'company-mode)
+(add-hook 'web-mode-hook 'prettier-mode)
+(add-hook 'web-mode-hook #'turn-on-smartparens-mode t)
+ ;; ...
+
+;; yasnippet
+(yas-global-mode 1)
+
+;; flycheck
+(global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; company-mode
+(global-company-mode)
+
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/")
+(yas-global-mode 1)
+
+(add-hook 'typescript-mode-hook 'prettier-mode)
+
+(provide 'init)
+;;; init.el ends here
