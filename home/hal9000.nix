c6b1807e2973ec92d42de1ffa0255d97b84d3e9a
Peter Hebden <peterhebden6@gmail.com>
2023-07-11 15:17:20 +0100

    obs with wayland

--- home/hal9000.nix
+++ home/hal9000.nix
@@ -12,6 +12,13 @@
 
   programs.swaylock.enable = true;
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+    ];
+  };
+
   home.file = {
     ".config/sway".source = .config/sway;
   };
